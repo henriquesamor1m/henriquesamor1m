8570f722362e6f835af4019b3201f5addd683f89
henriques amorim <143615655+henriquesamor1m@users.noreply.github.com>
2023-08-31 10:33:48 -0300

    Add files via upload

---
+++ kmCarro.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class kmCarro {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("digite a distancia em km: ");
+        float km= scan.nextFloat();
+
+        System.out.println("digite a quantidade de gasolida consumida pelo carro:  ");
+        float consumo= scan.nextFloat();
+
+        float kml= km/consumo;
+        System.out.println("Consumo: "+kml+" km/l");
+
+        if (kml<8){
+            System.out.println("VENDA O CARRO");
+        } else if (kml>=8 && kml<=14) {
+            System.out.println("ECONOMICO");
+        } else if (kml>12) {
+            System.out.println("SUPER ECONOMICO");
+        }else {
+            System.out.println("ERROR");
+        }
+    }
+}
