7dcb8787be0f37b99225e9ff4bd7a6c7ec151166
henriques amorim <143615655+henriquesamor1m@users.noreply.github.com>
2023-08-31 08:24:42 -0300

    Add files via upload

---
+++ classificao.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class aula {
+    public static void main(String args[]){
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("digite sua altura: ");
+        float altura= scan.nextFloat();
+
+        System.out.print("digite o seu peso: ");
+        float peso= scan.nextFloat();
+
+        if (altura<1.20 && peso<=60){
+            System.out.println("classificacao A");
+        } else if (altura>=1.20 && altura<=1.70 && peso<=60) {
+            System.out.println("classificacao B");
+        } else if (altura>1.70 && peso<=60) {
+            System.out.println("classificacao C");
+        } else if (altura<1.20 && peso>=60 && peso<=90) {
+            System.out.println("classificao D");
+        } else if (altura>=1.20 && altura<=1.70 && peso>=60 && peso<=90) {
+            System.out.println("classificao E");
+        } else if (altura>1.70 && peso>=60 && peso<=90) {
+            System.out.println("classificao F");
+        } else if (altura<1.20 && peso>90) {
+            System.out.println("classificacao G");
+        } else if (altura>=1.20 && altura<=1.70 && peso>90) {
+            System.out.println("classificao H");
+        } else if (altura>1.70 && peso>90) {
+            System.out.println("classificacao I");
+        }else {
+            System.out.println("INVALIDO");
+        }
+    }
+}
