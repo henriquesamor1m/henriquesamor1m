032dd64e415166866a1ba4bb8f8d549865ae4235
henriques amorim <143615655+henriquesamor1m@users.noreply.github.com>
2023-09-04 10:15:34 -0300

    Update venda.java

--- venda.java
+++ venda.java
@@ -4,8 +4,8 @@ public class venda {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
 
-       /* System.out.println("digite seu nome: ");
-        String nome= scan.nextLine(); */
+        System.out.println("digite seu nome: ");
+        String nome= scan.nextLine(); 
 
         System.out.println("digite o valor da venda mensal: ");
         double venda = scan.nextDouble();
